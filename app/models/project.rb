cd8e93fbc759f2f656086e02cf6443be1787ad0d
somethinrother <jamesneilson4@gmail.com>
2017-12-08 18:47:58 -0500

    Fix project end_date_is_after_start_date method so it actually rolls back a save

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,6 +8,8 @@ class Project < ActiveRecord::Base
   validate  :end_date_is_after_start_date
 
   def end_date_is_after_start_date
-    end_date >= start_date
+    if end_date <= start_date
+      errors.add(:end_date, "must be after start date")
+    end
   end
 end
