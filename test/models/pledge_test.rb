16a4ce8d981c94d8496a9ae8e80d33af93364004
somethinrother <jamesneilson4@gmail.com>
2017-12-11 17:29:00 -0500

    Refactor test for pledge owner vs project owner

--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -4,6 +4,7 @@ class PledgeTest < ActiveSupport::TestCase
 
   def setup
     @pledge = build(:pledge)
+    @project = @pledge.project
   end
 
   test 'A pledge can be created' do
@@ -13,16 +14,9 @@ class PledgeTest < ActiveSupport::TestCase
   end
 
   test 'owner cannot back own project' do
-    skip
-    owner = new_user
-    owner.save
-    project = new_project
-    project.user = owner
-    project.save
-    pledge = Pledge.new(dollar_amount: 3.00, project: project)
-    pledge.user = owner
-    pledge.save
-    assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
+    @pledge.user = @project.user
+    @pledge.save
+    assert @pledge.invalid?, 'Owner should not be able to pledge towards own project'
   end
 
 end
