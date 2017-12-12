fb701106179ae32cba12ff1f532254a10280feac
Eric Marrone <emarrone17@gmail.com>
2017-12-12 17:59:24 -0500

    Add method to project model to check start date is not in future. Add tests for method

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,10 +8,17 @@ class Project < ActiveRecord::Base
   validates :title, :user_id, :description, :goal, :start_date, :end_date, presence: true
   validates :goal, numericality: { :greater_than_or_equal_to => 1, message: " must be a number greater than zero." }
   validate :end_date_before_start_date?
+  validate :start_date_not_in_future?
 
   def end_date_before_start_date?
     if end_date <= start_date
       errors.add(:end_date, "must be after start date")
+    end
+  end
+
+  def start_date_not_in_future?
+    if start_date <= Time.now.utc
+      errors.add(:start_date, "must be in future")
     end
   end
 end
