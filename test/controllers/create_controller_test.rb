a4719e4746d449b571de1cf0d418b7b4b64508f5
somethinrother <jamesneilson4@gmail.com>
2017-12-12 16:28:00 -0500

    Fix issue in updates controller test

--- test/controllers/create_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CreateControllerTest < ActionDispatch::IntegrationTest
-  test "should get update" do
-    get create_update_url
-    assert_response :success
-  end
-
-end
