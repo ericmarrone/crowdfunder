e86114af319b296f226d48d104fa673e79ca1871
Eric Marrone <emarrone17@gmail.com>
2017-12-13 17:57:03 -0500

    Add edit functionality to comments on project show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,45 @@ class CommentsController < ApplicationController
   before_action :current_user
 
   def create
-    @project = Project.find(params[:id])
+    @project = Project.find(params[:project_id])
     @comment = Comment.new
     @comment.title = params[:comment][:title]
     @comment.comment = params[:comment][:comment]
     @comment.user = current_user
     @comment.project = @project
+
+    if @comment.save
+      redirect_to project_url(@project)
+    else
+      render '/projects/show'
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:project_id])
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:project_id])
+    @comment = Comment.find(params[:id])
+    @comment.title = params[:comment][:title]
+    @comment.comment = params[:comment][:comment]
+    @comment.user = current_user
+    @comment.project = @project
+
+    if @comment.save
+      redirect_to project_url(@project)
+    else
+      render '/projects/show'
+    end
   end
 
   def destroy
+    @project = Project.find(params[:project_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "You have successfully deleted the review."
+    redirect_to project_url(@project)
   end
 end
