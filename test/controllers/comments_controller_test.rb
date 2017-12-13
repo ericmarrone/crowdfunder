7e4ca6f12410d0739f24497aa239a01dd49ab736
Eric Marrone <emarrone17@gmail.com>
2017-12-13 17:59:14 -0500

    Remove comments controller tests generated by rails

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,22 +1,6 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    skip
-    get comments_create_url
-    assert_response :success
-  end
 
-  test "should get update" do
-    skip
-    get comments_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    skip
-    get comments_destroy_url
-    assert_response :success
-  end
 
 end
